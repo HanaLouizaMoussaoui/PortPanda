9842d925ea9d0397d452bcee124301d1694d7ba4
ryanwiwcharyk <ryanwiwcharyk@icloud.com>
2024-12-09 21:16:45 -0500

    worlds first manual merge

--- app.py
+++ app.py
@@ -15,18 +15,20 @@ scanner = nmap.PortScanner()
 def homepage():
     return render_template('index.html')
 
+
 @app.route("/results")
 def results():
     return render_template('results.html')
 
+
 @app.route("/scan", methods=['POST'])
 def scan():
     try:
-        protocol = request.form.get('protocol', 'tcp').lower() # default is tcp
-        start_port = request.form.get('start_port', '1') # default is 1
-        end_port = request.form.get('end_port', '500') # default is the max (65535)
-        single_port = request.form.get('single_port', False) # if user wants to scan only one port
-        ip_range = request.form.get('ip_range', 'www.megacorpone.com' )
+        protocol = request.form.get('protocol', 'tcp').lower()  # default is tcp
+        start_port = request.form.get('start_port', '1')  # default is 1
+        end_port = request.form.get('end_port', '500')  # default is the max (65535)
+        single_port = request.form.get('single_port', False)  # if user wants to scan only one port
+        ip_range = request.form.get('ip_range', 'www.megacorpone.com')
 
         # validating port inputs
         if not start_port.strip().isdigit():
@@ -35,13 +37,11 @@ def scan():
             start_port = int(start_port)
 
         if not end_port.strip().isdigit():
-            end_port = 500 # default
+            end_port = 500  # default
         else:
             end_port = int(end_port)
 
-        
-        hosts = [socket.gethostbyname('www.megacorpone.com'), socket.gethostbyname('www.google.com')]
-        
+        hosts_list = ip_range.split(',')
 
         if single_port:
             ports = end_port
@@ -50,26 +50,25 @@ def scan():
             if start_port < 1 or start_port > 65534:
                 return render_template('error.html', error="Start port must be between 1 and 65534."), 400
             if end_port < 2 or end_port > 65535:
-                return render_template('error.html', error= "End port must be between 2 and 65535."), 400
+                return render_template('error.html', error="End port must be between 2 and 65535."), 400
             if start_port > end_port:
                 return render_template('error.html', error="Start port cannot be greater than end port."), 400
             ports = f"{start_port}-{end_port}"
 
-        
-        results = scan_range(hosts, ports, protocol)
-        print(results)
+        results = scan_range(hosts_list, ports, protocol)
 
         open_ports_results = {}
         for host, host_results in results.items():
+            open_ports_results[host] = [result for result in host_results if
+                                        isinstance(result, dict) and 'open' in result.get('state', '')]
 
-            open_ports_results[host] = [result for result in host_results if isinstance(result,dict) and 'open' in result.get('state', '')]
+        enhanced_results = enhance_scan_results(open_ports_results)  # appends educational content to the results.
 
-        enhanced_results = enhance_scan_results(open_ports_results) # appends educational content to the results.
-
-        return render_template('results.html', enhanced_results=enhanced_results) #returning the results as a json object
+        return render_template('results.html', results=enhanced_results)  # returning the results as a json object
 
     except Exception as e:
         return render_template('error.html', error=f"Exception {e} was thrown."), 500
-    
+
+
 if __name__ == '__main__':
     app.run(debug=True)
